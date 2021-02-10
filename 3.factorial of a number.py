b466ef8d46b3ffcf42c5f83c3e680fcf4561c87f
Yuandao <36407106+MartinoBin@users.noreply.github.com>
2021-02-10 19:59:04 +0100

    Update 3.factorial of a number.py

--- 3.factorial of a number.py
+++ 3.factorial of a number.py
@@ -8,3 +8,32 @@ print('Factorial of', num, 'is',factorial(num))
 
 
 
+
+
+
+
+def factorial(n):
+    return 1 if n==1 or n==0 else n*factorial(n-1);
+num=int(input("Give a number:"))
+print("Factorial of",num,"is",factorial(num))
+
+
+
+def factorial(n):
+    if n<0:
+        return 0
+    elif n==0 or n==1:
+        return 1
+    else:
+        fact=1
+        while(n>1):
+            fact *=n
+            n-=1
+        return fact
+
+num1=input('Give a number:')
+
+print('Factorial of',num1,'is',factorial(num1))
+
+
+

0a59e48d00b2ff0b73b84483e49772c116e54e9f
Yuandao <36407106+MartinoBin@users.noreply.github.com>
2021-03-01 19:42:08 +0100

    Create 9.Find the length of a list.py

---
+++ 9.Find the length of a list.py
@@ -0,0 +1,23 @@
+#Python code to demonstrate 
+#length of list
+#using naive method
+
+#Initializing list
+
+test_list=[1,2,3,4,5,6]
+#printing test list
+print('The list is +str(test_list))
+     
+#Finding length of list
+#using loop
+#Initializing counter
+
+counter=0
+for i in test_list:
+      
+      #incrementing counter
+      counter=counter+1
+      
+#printing length of list
+print('Length of list using naive method is:'+str(counter))
+      

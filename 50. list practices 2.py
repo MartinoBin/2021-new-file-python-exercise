68e811c1a8512320b2bfa1fe92b9f7a2cc9d8957
Yuandao <36407106+MartinoBin@users.noreply.github.com>
2022-01-19 15:02:46 +0100

    Create 50. list practices 2.py

---
+++ 50. list practices 2.py
@@ -0,0 +1,28 @@
+print('\nSummary Table')
+
+num_string=['15','100','55','42']
+
+print('\nThe variable num_string is a', type(num_string))
+print('It contains the elements:',num_string)
+print('The element 15 is a',type(num_string[0]))
+
+
+num_string=[15,100,55,42]
+
+print('\nThe variable num_int is a', type(num_string))
+print('It contains the elements:',num_string)
+print('The element 15 is a',type(num_string[0]))
+
+
+num_string=[2.2,5.0,1.245,0.142857]
+
+print('\nThe variable num_int is a', type(num_string))
+print('It contains the elements:',num_string)
+print('The element 2.2 is a',type(num_string[0]))
+
+
+num_string=[[1,2,3],[4,5,6],[7,8,9]]
+
+print('\nThe variable num_int is a', type(num_string))
+print('It contains the elements:',num_string)
+print('The element',num_string[0],'is a',type(num_string[0]))
